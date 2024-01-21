aeee6c5a162de3b798cbf9d435ac0bb104b1e127
SamuelValentin974 <72002737+SamuelValentin974@users.noreply.github.com>
2024-01-22 02:45:42 +0400

    added a level 2 and a next zone

---
+++ script/levels.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var _nextScene : PackedScene = null
+@export var _isFinalLevel: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func _on_next_zone_body_entered(body):
+	if body is Player:
+		if _isFinalLevel || (_nextScene != null):
+			if _isFinalLevel:
+				get_tree().quit()
+			else:
+				get_tree().change_scene_to_packed(_nextScene)
